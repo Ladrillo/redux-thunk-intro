fed9965bf4ea0feda0e527fe067bfd113b8975a1
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-02-19 10:35:14 +0100

    wip

--- src/state/actionTypes.js
+++ src/state/actionTypes.js
@@ -1,3 +1,9 @@
 // STEP-2 DESIGN THE TYPES OF ACTIONS THE STATE MIGHT SUFFER
 export const INPUT_CHANGE = 'INPUT_CHANGE'
 export const RESET_INPUTS = 'RESET_INPUTS'
+// fetching friends
+export const FETCH_FRIENDS_START = 'FETCH_FRIENDS_START' // no state change
+export const SET_FETCHED_FRIENDS = 'SET_FETCHED_FRIENDS' // friends from API
+// spinner
+export const SPINNER_START = 'SPINNER_START'
+export const SPINNER_STOP = 'SPINNER_STOP'
