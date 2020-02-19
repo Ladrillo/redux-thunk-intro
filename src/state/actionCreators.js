9eff02a27cf878a86bfa35c0b3fc309071166958
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-02-19 09:55:11 +0100

    wip

--- src/state/actionCreators.js
+++ src/state/actionCreators.js
@@ -1,5 +1,7 @@
 import * as types from './actionTypes'
 
+const friendsAPI = 'http://localhost:4000/api/friends'
+
 // STEP-7 MAKE ACTION CREATORS THE COMPONENTS CAN USE DIRECTLY
 export function changeInput({ inputName, inputValue }) {
   return {
