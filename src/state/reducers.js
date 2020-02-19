c1e90ccb7758920e3a75c825328f701f55ddc85e
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-02-19 08:38:53 +0100

    cleanup

--- src/state/reducers.js
+++ src/state/reducers.js
@@ -1,17 +1,5 @@
 import * as types from './actionTypes'
 
-// {
-//   formValues: { // SLICE 1
-//     fname: '',
-//     lname: '',
-//   },
-//   friends: [    // SLICE 2
-//     { id: someId, fname: 'Jane', lname: 'Doe', married: false },
-//     { id: anotherId, fname: 'john', lname: 'Smith', married: false },
-//   ],
-//   isSpinnerOn: false,
-// }
-
 // STEP-3 CREATE ONE REDUCER FUNCTION PER SLICE OF STATE
 const initialStateForm = { fname: '', lname: '' }
 export function formReducer(state = initialStateForm, action) {
