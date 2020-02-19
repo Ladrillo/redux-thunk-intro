449b64b2adaff1b1aa8839fe0c2158ec424600fd
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-02-19 11:55:06 +0100

    wip

--- src/state/actionTypes.js
+++ src/state/actionTypes.js
@@ -4,6 +4,9 @@ export const RESET_INPUTS = 'RESET_INPUTS'
 // fetching friends
 export const FETCH_FRIENDS_START = 'FETCH_FRIENDS_START' // no state change
 export const SET_FETCHED_FRIENDS = 'SET_FETCHED_FRIENDS' // friends from API
+// creating new friend
+export const POST_FRIEND_START = 'POST_FRIEND_START' // no state change
+export const SET_POSTED_FRIEND = 'SET_POSTED_FRIEND' // add the friend to the redux
 // spinner
 export const SPINNER_START = 'SPINNER_START'
 export const SPINNER_STOP = 'SPINNER_STOP'
